7c2c7ef2ebae62c18c5615bf42dd2cf13b9d54f4
Alena Skornyakova <73646090+Alyonaskor@users.noreply.github.com>
2025-06-29 18:27:22 +0200

    1. Move folder auth, post, user to one folder modules 2. some changes for registration

--- learnify-api/src/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthResolver } from './auth.resolver';
-
-@Module({
-  providers: [AuthService, AuthResolver],
-  exports: [AuthService],
-})
-export class AuthModule {}
